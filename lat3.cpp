aeb6c052084582f9b982f0b266ac084fd06e41b0
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:48:20 +0700

    Add files via upload

---
+++ lat3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int a,b;
+cout<<"masukan bilangan a:";
+cin>>a;
+cout<<"masukan bilangan b:";
+cin>>b;
+if(a<b){
+    cout<<"b terbesar\n<<";
+}else if(a>b){
+cout<<"a terbesar\n<<";
+}else
+cout<<"error"<<endl;
+return 0;
+}
